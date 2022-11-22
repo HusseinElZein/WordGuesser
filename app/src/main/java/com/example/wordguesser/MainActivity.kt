e7655c0f9cc74300832e30e31dcef25bec5c34f7
Hussein El-Zein <husseinelzeinprivat@gmail.com>
2022-11-22 23:07:46 +0100

    changed some colors

--- app/src/main/java/com/example/wordguesser/MainActivity.kt
+++ app/src/main/java/com/example/wordguesser/MainActivity.kt
@@ -8,10 +8,13 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.wordguesser.Navigation.NavigatorSetup
 import com.example.wordguesser.ui.theme.WordGuesserTheme
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,6 +26,19 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
+
+                    val systemUiController = rememberSystemUiController()
+                    SideEffect {
+                        systemUiController.setStatusBarColor(
+                            color = Color.Transparent,
+                            darkIcons = false
+                        )
+                        systemUiController.setNavigationBarColor(
+                            color = Color(0xFFD2D2D2),
+                            darkIcons = false
+                        )
+                    }
+
                     App()
                 }
             }
